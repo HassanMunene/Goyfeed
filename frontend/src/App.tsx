297e030317c2a6a1080ee5d959135cf47dbaff69
HassanMunene <awanzihassan@gmail.com>
2025-04-08 11:09:34 +0300

    included the Home page as the root path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Home from './pages/Home'
 import './App.css'
 
 function App() {
-  return (
-    <div className='text-red-500'>GoyFeed application</div>
-  )
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<h1>About</h1>} />
+				<Route path="/contact" element={<h1>Contact</h1>} />
+			</Routes>
+		</BrowserRouter>
+	)
 }
 
 export default App
