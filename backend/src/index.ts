d1dde90ea4796aa72c90e215b9a968af453064be
HassanMunene <awanzihassan@gmail.com>
2025-04-08 10:28:10 +0300

    First Commit for Goy feed

---
+++ backend/src/index.ts
@@ -0,0 +1,8 @@
+import { Express } from "express";
+import { ApolloServer } from "apollo-server-express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+//load envronement variables from .env file
+dotenv.config();
+
