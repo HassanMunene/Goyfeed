f4bbf7d025452c69ef3228dc8f1b2e9973685d33
HassanMunene <awanzihassan@gmail.com>
2025-04-08 22:22:02 +0300

    making the main layout mobile friendly

--- frontend/src/components/Layout/MainLayout.tsx
+++ frontend/src/components/Layout/MainLayout.tsx
@@ -1,25 +1,40 @@
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "../sidebars/Sidebar";
 import RightSidebar from "../sidebars/RightSidebar";
 
 
 const MainLayout = () => {
+    // we need to find a way we can sense the scrolling to add some stylings
+    const [scrolled, setScrolled] = useState(false);
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrolled(window.scrollY > 10);
+        }
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        }
+    }, []);
+
     return (
         <div className="flex min-h-screen bg-white text-black">
-            {/* Left Sidebar */}
-            <div className="w-[275px] border-r border-x-border dark:border-x-border flex-shrink-0 sticky top-0 h-screen overflow-y-auto">
+            {/* Left Sidebar when scrolled we will have a subtle shadow */}
+            <div className={`w-[275px] border-r border-[#f0f2f5] flex-shrink-0 sticky top-0 h-screen z-20  ${scrolled ? 'shadow-sm' : ''}`}>
                 <Sidebar />
             </div>
 
-            {/* Main Content */}
-            <div className="flex-1 border-r border-x-border dark:border-x-border min-h-screen">
-                <Outlet />
-            </div>
+            {/* Main Content this section should be flexible*/}
+            <main className="flex-1 border-r border-[#f0f2f5] min-h-screen">
+                <div className="max-w-2xl mx-auto p-0 sm:p-4">
+                    <Outlet />
+                </div>
+            </main>
 
-            {/* Right Sidebar */}
-            <div className="w-[350px] sticky top-0 h-screen overflow-y-auto hidden xl:block">
+            {/* Right Sidebar - Conditional display only on lg screens and above */}
+            <aside className="hidden lg:block w-[350px] sticky top-0 h-screen overflow-y-auto no-scrollbar">
                 <RightSidebar />
-            </div>
+            </aside>
         </div>
     );
 };
